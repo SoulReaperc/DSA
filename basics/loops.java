abedc10bc1b7fa5a6ee7b00bc054a1c4a132d62a
SoulReaper <darshil.kumar31@gmail.com>
2026-06-24 17:27:07 +0530

    loops learned

---
+++ basics/loops.java
@@ -0,0 +1,23 @@
+public class loops{
+    public static void main(String[] args){
+        //loops are a control flow structure that repeats a specific block of code until a certain condition is met.
+        // types of loops 
+        // 1. for
+        // 2. while
+        // 3. do-while
+        // 4. enhanced for 
+        System.out.println("For loop");
+        for( int i = 1 ; i <= 5; i++){ // count till the number 5
+            System.out.println("Count " + i);
+        } 
+
+        System.out.println("while loop");
+        int a = 0;
+        while(a <= 5){ // count till 5 ,but always runs once regardless of the condition
+            System.out.println("Count " + i);
+        }
+
+        System.out.println("while loop");
+        do
+    }
+}
