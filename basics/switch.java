804f6326ac994e44fbee0eadb5132c0c91d960f4
SoulReaper <darshil.kumar31@gmail.com>
2026-06-25 20:57:15 +0530

    switches learned

---
+++ basics/switch.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class switcht{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("select a number 1 to 7");
+        int a = sc.nextInt();
+        switch(a){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thusday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("whaaat ??????");
+                break;
+
+        }
+        sc.close();
+    }
+}
