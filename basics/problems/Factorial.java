514d9d66705d69915cdd522cc33025949a8b1f5a
SoulReaper <darshil.kumar31@gmail.com>
2026-06-27 20:47:19 +0530

    factorial done

---
+++ basics/problems/Factorial.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class factorial{
+    static int factor(int num){
+        if(num == 0){
+            return 1;
+        }
+        return num * factor(num-1);
+    } 
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your number: ");
+        int num = sc.nextInt();
+        System.out.println(factor(num));
+    }
+}
